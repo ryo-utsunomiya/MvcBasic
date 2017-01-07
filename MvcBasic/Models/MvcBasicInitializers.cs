5029e5cd69aa2cdcb49b8c81ea21794f7ec98984
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-07 19:21:06 +0900

    Create Member model

---
+++ MvcBasic/Models/MvcBasicInitializers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcBasic.Models
+{
+    public class MvcBasicInitializers : DropCreateDatabaseAlways<MvcBasicContext>
+    {
+        protected override void Seed(MvcBasicContext context)
+        {
+            context.Members.Add(new Member
+            {
+                Name = "Rio",
+                Email = "rio@example.com",
+                Birth = DateTime.Parse("1980-06-25"),
+                Married = false,
+                Memo = "I love piano"
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
